45c698e4dba551ecdbaaf9567d8af19bac0ca9d2
0xyd <davisfreeman1015@gmail.com>
2021-02-14 13:40:49 +0100

    REFACTOR
    Refactor test code

---
+++ converter/tflite/test_helpers.py
@@ -0,0 +1,90 @@
+import sys
+import tensorflow as tf
+
+import pprint
+import pathlib
+import subprocess
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
+
+sys.path.append('../')
+
+from cgen.graph import iKannGraph, FlatbufferToDict
+from cgen.snippet import CSnippetGenerator
+
+def convert_keras_to_tflite(kerasModel):
+	'''
+	Convert a keras model to tflite model
+
+	kerasModel:
+	A keras model is being converted.
+	'''
+
+	converter = tf.lite.TFLiteConverter.from_keras_model(kerasModel)
+	tfliteModel = converter.convert()
+	return tfliteModel
+
+def build_ikann_graph(tfliteModel):
+	'''
+	Build a ikann graph from tflite model.
+
+	tfliteModel:
+	The binary of tfliteModel
+	'''
+	ikannGraph = iKannGraph()
+	ikannGraph.parse(tfliteModel)
+	return ikannGraph
+
+def generate_c_code(ikannGraph):
+	'''
+	Generate c code from ikannGraph
+
+	ikannGraph: <dict>
+	ikannGraph contains layers of different operations
+	'''
+	cGenerator = CSnippetGenerator(templatePath='../../../templates')
+	cGenerator.build_code(ikannGraph)
+
+def invoke_tensorflow_lite(tfliteModel, inputData):
+	'''
+	Invoke tflite model to see output result
+
+	tfliteModel:
+	The binary of tfliteModel
+
+	inputData: <np.ndarray>
+	Input data
+	'''
+	interpreter = tf.lite.Interpreter(model_content=tfliteModel)
+	interpreter.allocate_tensors()
+	inputDetails = interpreter.get_input_details()
+	outputDetails = interpreter.get_output_details()
+
+	interpreter.set_tensor(inputDetails[0]['index'], inputData)
+	interpreter.invoke()
+	outputResult = interpreter.get_tensor(outputDetails[0]['index'])
+	return outputResult
+
+def build_and_execute_c_code():
+	'''
+	Build and execute the generated c code.
+
+	'''
+	subprocess.run(['make'], check=True)
+	subprocess.run(['./hello'], check=True)
+
+def compare_results_between_tflite_and_ikann(tfliteOutput):
+	'''
+	Compare inferenced results between tflite model and ikann model
+
+	tfliteOutput: <np.ndarray>
+	Output result of tflite model
+	'''
+	with open('model_output.txt') as f:
+		ikannOutput = [float(v) for v in f.readline().split(',') if len(v) > 0]
+	for ov, cv in zip(tfliteOutput, ikannOutput):
+		if abs(ov-cv) > 1e-5:
+			raise ValueError(f"output results between tflite and ikann model is different! tflite: {ov} ; ikann: {cv}")
+
