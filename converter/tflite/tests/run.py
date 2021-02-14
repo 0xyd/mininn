e951c733fb404aa0bde9985541e7ea83efc5662f
0xyd <davisfreeman1015@gmail.com>
2021-02-14 17:27:00 +0100

    REFACTOR TESTCASE FCNN_SIGMOID
    Refactor code in fcnn_sigmoid so that it can be covered by global unittest

--- converter/tflite/tests/run.py
+++ converter/tflite/tests/run.py
@@ -3,10 +3,15 @@ import sys
 import unittest
 
 from conv2d.test import Conv2dTestCase
+from fcnn_relu.test import FcnnReluTestCase
+from fcnn_sigmoid.test import FcnnSigmTestCase
+
 
 def test_all():
 	suite = unittest.TestSuite()
 	suite.addTest(Conv2dTestCase('test_single_conv2d'))
+	suite.addTest(FcnnReluTestCase('test_codegen'))
+	suite.addTest(FcnnSigmTestCase('test_codegen'))
 	return suite
 
 if __name__ == '__main__':
