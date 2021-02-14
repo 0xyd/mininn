8de50c61160ed95f3fed353e6158a2f43ed85a3d
0xyd <davisfreeman1015@gmail.com>
2021-02-14 17:37:08 +0100

    REFACTOR TANH TEST CASE
    Refactor tanh test case

--- converter/tflite/tests/fcnn_sigmoid/test.py
+++ converter/tflite/tests/fcnn_sigmoid/test.py
@@ -24,46 +24,46 @@ pp = pprint.PrettyPrinter(indent=4)
 class FcnnSigmTestCase(unittest.TestCase):
 
 
-    def test_codegen(self):
+	def test_codegen(self):
 
-        model = keras.Sequential([
-            keras.layers.InputLayer(input_shape=(5)),
-            keras.layers.Dense(5, activation='sigmoid'),
-            keras.layers.Dense(10, activation='sigmoid'),
-            keras.layers.Dense(5, activation='sigmoid'),
-            keras.layers.Dense(10, activation='sigmoid'),
-            keras.layers.Dense(5, activation='sigmoid'),
-            keras.layers.Softmax()
-        ])
+		model = keras.Sequential([
+			keras.layers.InputLayer(input_shape=(5)),
+			keras.layers.Dense(5, activation='sigmoid'),
+			keras.layers.Dense(10, activation='sigmoid'),
+			keras.layers.Dense(5, activation='sigmoid'),
+			keras.layers.Dense(10, activation='sigmoid'),
+			keras.layers.Dense(5, activation='sigmoid'),
+			keras.layers.Softmax()
+		])
 
-        tfliteModel = convert_keras_to_tflite(model)
-        ikannGraph = build_ikann_graph(tfliteModel)
+		tfliteModel = convert_keras_to_tflite(model)
+		ikannGraph = build_ikann_graph(tfliteModel)
 
-        # Template path looks different when caller of test is local or global.
-        # First condition is when we test the current test only
-        # Second condition is when we test all cases
-        if os.path.exists('../../../templates'):
-            generate_c_code(
-                ikannGraph, 
-                templatePath='../../../templates',)
-        else:
-            generate_c_code(
-                ikannGraph, 
-                templatePath='../../templates', 
-                outputPath='fcnn_sigmoid')
+		# Template path looks different when caller of test is local or global.
+		# First condition is when we test the current test only
+		# Second condition is when we test all cases
+		if os.path.exists('../../../templates'):
+			generate_c_code(
+				ikannGraph, 
+				templatePath='../../../templates',)
+		else:
+			generate_c_code(
+				ikannGraph, 
+				templatePath='../../templates', 
+				outputPath='fcnn_sigmoid')
 
-        inputData = np.array([[1., 2., 3., 4., 5.]], dtype=np.float32)
+		inputData = np.array([[1., 2., 3., 4., 5.]], dtype=np.float32)
 
-        outputResult = invoke_tensorflow_lite(
-            tfliteModel, inputData).flatten().round(decimals=8)
+		outputResult = invoke_tensorflow_lite(
+			tfliteModel, inputData).flatten().round(decimals=8)
 
-        build_and_execute_c_code(codePath='fcnn_sigmoid')
+		build_and_execute_c_code(codePath='fcnn_sigmoid')
 
-        # Compare output results
-        compare_results_between_tflite_and_ikann(outputResult)
+		# Compare output results
+		compare_results_between_tflite_and_ikann(outputResult)
 
-        os.remove('model_output.txt')
+		os.remove('model_output.txt')
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
 
--- converter/tflite/tests/run.py
+++ converter/tflite/tests/run.py
@@ -4,6 +4,7 @@ import unittest
 
 from conv2d.test import Conv2dTestCase
 from fcnn_relu.test import FcnnReluTestCase
+from fcnn_tanh.test import FcnnTanhTestCase
 from fcnn_sigmoid.test import FcnnSigmTestCase
 
 
@@ -12,10 +13,9 @@ def test_all():
 	suite.addTest(Conv2dTestCase('test_single_conv2d'))
 	suite.addTest(FcnnReluTestCase('test_codegen'))
 	suite.addTest(FcnnSigmTestCase('test_codegen'))
+	suite.addTest(FcnnTanhTestCase('test_codegen'))
 	return suite
 
 if __name__ == '__main__':
-	print(f"os.path.realpath(__file__) : {os.path.realpath(__file__) }")
-
 	runner = unittest.TextTestRunner()
 	runner.run(test_all())
